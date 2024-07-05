01ab2d48f7565367c9f6c9d7a66c9f7445b366d1
Motale Hosssain Emon <motalebhossainemon@gmail.com>
2024-07-06 01:30:05 +0600

    SecondCommit

---
+++ first.js
@@ -0,0 +1,46 @@
+console.log("Emon Hossain");
+let sum=50+29;
+console.log(sum);
+
+let sub=26-2;
+console.log(sub);
+
+let mul=3*4;
+console.log(mul);
+
+let div=27/3;
+console.log(div);
+
+console.log("Hi there !");
+
+//console.log("I love Java Script");
+
+full_name="Emon Hossain";
+age=24;
+price=99.99;
+radius=145;
+
+isFollow= false;
+
+console.log(isFollow);
+
+console.log(price);
+
+/*sum=55+3;
+
+console.log(sum);
+
+age=33;
+*/
+
+let a=6;
+let b=3;
+
+console.log("a=",a, "& b =",b);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a**b=",a**b); //it need to power
+console.log("a++ =",a++);
+console.log("--a =", --a);
